a4bc582d352858d2c2c60650cda9bf38b60dd7c3
lucasbaquino <lucasbaquino@icloud.com>
2020-08-31 21:56:16 -0300

    Resolve the error about type of User

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -2,41 +2,38 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
-import uploadConfig from '../config/upload';
 import User from '../models/User';
+import uploadConfig from '../config/upload';
+// import AppError from '../errors/AppError';
 
 interface Request {
   user_id: string;
   avatarFilename: string;
 }
 
-class UpdateAvatarService {
+class UpdateUserAvatarService {
   public async execute({ user_id, avatarFilename }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
-
-    const user = await usersRepository.findOne(user_id);
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar');
+      throw new Error('Only authenticated users change avatar');
     }
 
     if (user.avatar) {
       // Deletar avatar anterior
+      const userAvatarPath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExist = await fs.promises.stat(userAvatarPath);
 
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
+      if (userAvatarFileExist) {
+        await fs.promises.unlink(userAvatarPath);
       }
-
-      user.avatar = avatarFilename;
-
-      await usersRepository.save(user);
-
-      return user;
     }
+
+    user.avatar = avatarFilename;
+    await userRepository.save(user);
+    return user;
   }
 }
 
-export default UpdateAvatarService;
+export default UpdateUserAvatarService;
