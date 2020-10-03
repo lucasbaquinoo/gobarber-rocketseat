bf76da527c259cab735df1dd5c52a3c078bcf004
lucasbaquino <lucasbaquino@icloud.com>
2020-10-03 16:41:21 -0300

    Add the structure to Forgot Password service

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,22 +1,43 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUserRepository from '../repositories/IUsersRepository';
+import IUserTokenRepository from '../repositories/IUserTokenRepository';
 
 // import User from '../infra/typeorm/entities/User';
 
-// interface IRequest {
-//   email: string;
-// }
+interface IRequest {
+  email: string;
+}
 
 @injectable()
 class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUserRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokenRepository,
   ) {}
 
-  public async execute(): Promise<void> {}
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
 }
 
 export default SendForgotPasswordEmailService;
